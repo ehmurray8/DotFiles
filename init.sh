4831d6c1849dc0c05e7f0e6dee4a3abe8f6d1848
Emmet Murray <ehmurray8@gmail.com>
2019-06-09 13:10:06 -0500

    Added a script for easily creating desktop entries for new apps on linux machines.

--- init.sh
+++ init.sh
@@ -41,6 +41,7 @@ if [ "$machine" = "Mac" ]; then
 elif [ "$machine" = "Linux" ]; then
     filesToLink[${#filesToLink[*]}]=$".Xmodmap"
     filesToLink[${#filesToLink[*]}]=$".xinitrc"
+    filesToLink[${#filesToLink[*]}]=$"add_app"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 

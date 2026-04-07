ee5a612083d0bd072324cc7aa76a016d9152262b
Emmet Murray <ehmurray8@gmail.com>
2026-04-07 10:29:40 -0600

    Prefer git for neovim treesitter installs.

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,7 @@
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
+require("nvim-treesitter.install").prefer_git = true
+
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
 	ensure_installed = {
